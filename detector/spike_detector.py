49346425b3aa70eb034693fe3fc735095b90f9dc
Mihai Bivol <mm.bivol@gmail.com>
2013-06-20 18:54:55 +0300

    Break detect_anomalies method in SpikeDetector into smaller ones

--- detector/spike_detector.py
+++ detector/spike_detector.py
@@ -29,12 +29,61 @@ class SpikeDetector(Detector):
 
         return min(prio, 1.0)
 
-
     @classmethod
     def _get_basic_spikes(cls, timeseries, timestamps, treshold):
         """Returns basic spikes that are usually smoothed"""
         return { timestamps[i]:cls._get_basic_spike_prio(timeseries[i], treshold) \
                  for i in range(len(timeseries)) if timeseries[i] > treshold }
+
+    @classmethod
+    def _get_SAX_spikes(cls, timeseries, timestamps, treshold):
+        """
+        Returns spikes counting how many times a timestamp is a maximum
+        in a SAX conversion
+        """
+
+        # Seconds bethween measurements
+        retention = (timestamps[-1] - timestamps[0]) / len(timestamps)
+
+        # Number of entries per window
+        entries_per_word = cls.WINDOW_SECONDS_COUNT / retention
+
+        num_windows = len(timeseries) / entries_per_word
+        window_size = len(timeseries) / num_windows
+
+        num_symbols = window_size * retention / cls.SECONDS_PER_SYMBOL
+
+        sax_generator = SAX(wordSize = num_symbols, alphabetSize = cls.ALPHABET_SIZE)
+
+        symbols_per_datapoint = int(round(cls.SECONDS_PER_SYMBOL / float(retention)))
+
+        # Convert timeseries into SAX notation
+        words, intervals = sax_generator.sliding_window(timeseries, num_windows, .8)
+
+        # Times index i is a maximal value
+        maximum_count = {i: 0 for i in xrange(len(timeseries))}
+        # Times index i is passed by a window
+        window_count = {i: 0 for i in xrange(len(timeseries))}
+
+        # Count in how many windows a timestamp is a local maximum
+        for i in xrange(len(words)):
+            word = words[i]
+            interval = intervals[i]
+
+            for j in xrange(len(word)):
+                index = j * symbols_per_datapoint + interval[0]
+                if word[j] == string.ascii_lowercase[cls.ALPHABET_SIZE - 1]:
+                    maximum_count[index] += 1
+                window_count[index] += 1
+
+        spikes = { }
+        for key, value in maximum_count.iteritems():
+            if value == window_count[key] and value and \
+               timeseries[key] > treshold:
+                   val = timeseries[key]
+                   spikes[timestamps[key]] = cls._get_basic_spike_prio(val, treshold)
+
+        return spikes
 
     @classmethod
     def _get_top_spikes(cls, spikes, duration):
@@ -75,48 +124,14 @@ class SpikeDetector(Detector):
         # Smoooth data
         cls.smooth_data(timeseries, 2)
 
-        # Seconds bethween measurements
-        retention = (timestamps[-1] - timestamps[0]) / len(timestamps)
+        # Get maxima in unsmoothed timeseries
+        basic_spikes = cls._get_basic_spikes(orig_timeseries, timestamps, 5 * max_value)
 
-        # Number of entries per window
-        entries_per_word = cls.WINDOW_SECONDS_COUNT / retention
+        # Get spikes using SAX conversion in the smoothed timeseries
+        SAX_spikes = cls._get_SAX_spikes(timeseries, timestamps, max_value)
 
-        num_windows = len(timeseries) / entries_per_word
-        window_size = len(timeseries) / num_windows
-
-        num_symbols = window_size * retention / cls.SECONDS_PER_SYMBOL
-
-        sax_generator = SAX(wordSize = num_symbols, alphabetSize = cls.ALPHABET_SIZE)
-
-        symbols_per_datapoint = int(round(cls.SECONDS_PER_SYMBOL / float(retention)))
-
-        # Convert timeseries into SAX notation
-        words, intervals = sax_generator.sliding_window(timeseries, num_windows, .8)
-
-        # Times index i is a maximal value
-        maximum_count = {i: 0 for i in xrange(len(timeseries))}
-        # Times index i is passed by a window
-        window_count = {i: 0 for i in xrange(len(timeseries))}
-
-        for i in xrange(len(words)):
-            word = words[i]
-            interval = intervals[i]
-
-            for j in xrange(len(word)):
-                index = j * symbols_per_datapoint + interval[0]
-                if word[j] == string.ascii_lowercase[cls.ALPHABET_SIZE - 1]:
-                    maximum_count[index] += 1
-                window_count[index] += 1
-
-        treshold = max_value
-
-        spikes = cls._get_basic_spikes(orig_timeseries, timestamps, 5 * treshold)
-
-        for key, value in maximum_count.iteritems():
-            if value == window_count[key] and value and \
-               timeseries[key] > treshold:
-                   val = timeseries[key]
-                   spikes[timestamps[key]] = cls._get_basic_spike_prio(val, treshold)
+        #create merged dict
+        spikes = dict(basic_spikes, **SAX_spikes)
 
         return cls._get_top_spikes(spikes, cls.WINDOW_SECONDS_COUNT)
 
